e4b8703fd51236e427a019c8fe0b8e2d24964f25
mridul56 <mridulgupta56@gmail.com>
2020-07-20 02:08:49 +0530

    added view for testing

---
+++ Sources/GeekScreen/GeekScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mridul Gupta on 20/07/20.
+//
+
+import Foundation
+import SwiftUI
+
+public struct ContentView: View {
+    @available(OSX 10.15.0, *)
+    public var body: some View {
+        Text("Hello World")
+    }
+}
+
+public struct ContentView_Previews: PreviewProvider {
+    @available(OSX 10.15.0, *)
+    public static var previews: some View {
+        ContentView()
+    }
+}
